bd9d7feb16ab1c81a0fd029e743c06b56a109fe6
David <kanter.dev@gmail.com>
2019-04-30 14:09:07 -0400

    Documentation Intermediate

--- PoliticsAndWar.ts
+++ PoliticsAndWar.ts
@@ -5,32 +5,39 @@ import axios from "axios";
 import * as Functions from "./functions";
 
 
-
+/**
+ * @public @class @export
+ * Main Class for Library. See Constructor Documentation.
+ */
 class PoliticsAndWar
 {
+    /**Holds API key for referencing API. */
     private key: string;
-    private password: string;
+    /** Readonly field defined on Object creation. Defaults to "pnw".
+     * Cannot be accessed from the object, if needed, store in client code.
+    */
+    private readonly password: string;
     private url: object = {};
 
     // Function Declarations
-    public allCities: any = Functions.allCities;
-    public alliance: any = Functions.alliance;
-    public allianceMembers: any = Functions.allianceMembers;
-    public alliances: any = Functions.alliances;
-    public applicants: any = Functions.applicants;
-    public bank: any = Functions.bank;
-    public city: any = Functions.city;
-    public nation: any = Functions.nation;
-    public nationsMilitary: any = Functions.nationsMilitary;
-    public nations: any = Functions.nations;
-    public tradeHistory: any = Functions.tradeHistory;
-    public tradePrice: any = Functions.tradePrice;
-    public war: any = Functions.war;
+    public allCities: Function = Functions.allCities;
+    public alliance: Function = Functions.alliance;
+    public allianceMembers: Function = Functions.allianceMembers;
+    public alliances: Function = Functions.alliances;
+    public applicants: Function = Functions.applicants;
+    public bank: Function = Functions.bank;
+    public city: Function = Functions.city;
+    public nation: Function = Functions.nation;
+    public nationsMilitary: Function = Functions.nationsMilitary;
+    public nations: Function = Functions.nations;
+    public tradeHistory: Function = Functions.tradeHistory;
+    public tradePrice: Function = Functions.tradePrice;
+    public war: Function = Functions.war;
 
     /**
+     * @public @constructor
      * @param {String} key The key the user would like to use for calling the api.
-     * @param {String} password A password to perform certain operations & view cached key.
-     * @ throws {KeyTypeException} Thrown when the provided key is not of type string.
+     * @param {String} password A password to perform certain operations & view stored key.
      */
     public constructor(key: string, password: string = "pnw")
     {
@@ -44,6 +51,12 @@ class PoliticsAndWar
         }
     }
 
+    /**
+     * @public @method genURL Returns a generated link with entered parameters.
+     * @param name Name of the api to generate a link for, as per api.json
+     * @param id ID or Params to insert into link generation.
+     * @returns {String}
+     */
     public genURL(name: string, id?: number | string): string
     {
         let link: string = this.url[name];
@@ -55,6 +68,11 @@ class PoliticsAndWar
         return link;
     }
 
+    /**
+     * @public @method showKey Returns the stored API key being used for calls.
+     * @param {String} password
+     * @returns {String}
+     */
     public showKey(pass: string): string
     {
         if (pass == this.password)
@@ -63,15 +81,20 @@ class PoliticsAndWar
             return "Password Incorrect";
     }
 
+    /**
+     * @public @void @method setKey Sets the API key.
+     * @param {String} key The desired key setting.
+     */
     public setKey(key: string): void
     { this.key = key }
 
 
     /**
-     * Performs a raw api call and returns a Promise with a useful part of the result.
-     * @async
+     * Performs a raw api call and returns a Promise object with a useful result.
+     * @public @async @method rawApiCall
      * @param {String} api Name of the API to call (in api.json)
      * @param {String} id The ID to use for link generation, or all params in string format.
+     * @returns {Promise} Success - Returns JSON; Reject - Returns Reason
      */
     public async rawApiCall(api: string, id: string = ""): Promise<any>
     {
--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# Politics and War for JavaScript (pnw4js) V1
-## A simple, lightweight, type safe Politics&War library written in TypeScript for nodejs and commonjs.
+# pnw4js
 
+[pnw4js](https://github.com/canterbotto/pnw4js) is a simple, type safe, promise-based JavaScript library for accessing the [Politics & War](http://politicsandwar.com/) API. It is written in TypeScript for CommonJS, NodeJS and TypeScript. Compiles to ES5.
+
+## Installation
+
+The easiest way to install pnw4js is using [npm](https://www.npmjs.com/) in your project directory.
+
+```bash
+cd project_dir
+npm install pnw4js
+```
+
+For greater flexibility — such as the ability to edit the code or manually configure the APIs — you are able to manually clone the project from the github. In your project directory, you will want to clone in the files 
+```bash
+cd project_dir
+git clone # Project from Github
+npm install # Installs all dependencies
+npm install typescript # for compiling github project
+tsc
+```
+You will then see a `dist` folder which contains .js files. Copy the entire contents of the folder into your project folder. To edit the files, you must perform any edits in `.ts` source files and then recompile using `tsc`.
+
+## Usage
+For standard installation using npm, the following code outlines usage. 
+##### NodeJS:
+```js
+const PoliticsAndWar = require("pnw4js");
+let pnw = new PoliticsAndWar("key", "password");
+
+let nationCall = await pnw.nation(64060); // test call
+```
+##### CommonJS:
+```js
+import PoliticsAndWar from "pnw4js";
+let pnw = new PoliticsAndWar("key", "password");
+
+let nationCall = await pnw.nation(64060);
+```
+This library is compatible with TypeScript. Please see Documentation for in-depth documentation. The `PoliticsAndWar(key, [pass])` constructor requires a string parameter for the key, and the default password is `pnw`. The password is `readonly`, however `key` can be updated using the `setKey(key)` method.
+
+## Documentation
+
+
+## Testing & Updates
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
